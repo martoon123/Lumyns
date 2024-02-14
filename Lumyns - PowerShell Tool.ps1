82f13f23a62f5169c09aa0b20b20367e45d349a4
martoon123 <martoon123@gmail.com>
2024-02-14 10:23:31 +0200

    Update Lumyns - PowerShell Tool.ps1

--- Lumyns - PowerShell Tool.ps1
+++ Lumyns - PowerShell Tool.ps1
@@ -23,17 +23,22 @@ $hostname = hostname
 while($true) {
     Write-Host ""
     Write-Host "Lumyns - IT PowerShell Tool" -ForegroundColor Magenta
+    $pwd = Get-Location
+    Write-Host "Working Location: " $pwd -ForegroundColor DarkGray
     Write-Host "----------------------------------" -ForegroundColor DarkGray
     systeminfo | findstr /B /C:"Host Name" /B /C:"Domain" /B /C:"OS Name" /B /C:"OS Version" /B /C:"System Manufacturer" /B /C:"System Model" /B /C:"System Type" /B /C:"Total Physical Memory" /B /C:"System Locale" /B /C:"Input Locale" /B /C:"Time Zone"
     Write-Host "----------------------------------" -ForegroundColor DarkGray
-    $pwd = Get-Location
-    Write-Host "Working Location: " $pwd
+    Write-Host ""
+    Write-Host "General Menu" -ForegroundColor Magenta
+    Write-Host "[1] System Details - Installed Apps, Printers, Network Adapaters, etc..." -ForegroundColor Yellow
+    Write-Host "[2] Windows Update - Cleanup/Fix/Update." -ForegroundColor Yellow
+    Write-Host "[3] Windows Operation System - Repair/Cleanup/Check." -ForegroundColor Yellow
+    Write-Host "[0] Exit" -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host "Nervio Menu" -ForegroundColor Magenta
     Write-Host "----------------------------------" -ForegroundColor DarkGray
-    Write-Host "Menu" -ForegroundColor Magenta
-    Write-Host "1. System Details - Installed Apps, Printers, Network Adapaters, etc..." -ForegroundColor Yellow
-    Write-Host "2. Windows Update - Cleanup/Fix/Update." -ForegroundColor Yellow
-    Write-Host "3. Windows Operation System - Repair/Cleanup/Check." -ForegroundColor Yellow
-    Write-Host "0. Exit" -ForegroundColor Yellow
+    Write-Host "[nerv-all] Configuration for general workstation." -ForegroundColor Yellow
+    Write-Host "[nerv-nas] Configuration for NAS workstation." -ForegroundColor Yellow
     Write-Host ""
 
     # Prompt for a string input
@@ -92,6 +97,49 @@ while($true) {
             DISM /Online /Cleanup-Image /ScanHealth
             Write-Host "Fix Windows image corruptions." -ForegroundColor Red
             DISM /Online /Cleanup-Image /RestoreHealth /Source:repairSource\install.wim
+            Write-Host "The process is completed!" -ForegroundColor Cyan
+        }
+        "nerv-all" {
+            Write-Host "Set Country/Region to Hebrew (Israel)" -ForegroundColor Red
+            Set-WinSystemLocale he-IL
+
+            Write-Host "Set Time Zone to Israel" -ForegroundColor Red
+            Set-TimeZone -Id "Israel Standard Time"
+            
+            Write-Host "Set Languages Hebrew and English (Default)" -ForegroundColor Red
+            Set-WinUserLanguageList en-US,he-IL -Force
+
+            Write-Host "Set Display Language" -ForegroundColor Red
+            Set-WinUILanguageOverride -Language en-US
+
+            Write-Host "Download and update Microsoft Store Library" -ForegroundColor Red
+            start ms-windows-store://downloadsandupdates
+            $(Write-Host "Press any key to continue..." -ForegroundColor Green -NoNewLine; Read-Host)
+
+            Write-Host "Make sure that the winget link is: https://cdn.winget.microsoft.com/cache" -ForegroundColor Red
+            winget source list
+            $(Write-Host "Press any key to continue..." -ForegroundColor Green -NoNewLine; Read-Host)
+
+            Write-Host "Installing all packages..." -ForegroundColor Red
+            winget install Microsoft.PowerShell --source winget --silent
+            winget install Microsoft.WindowsTerminal --source winget --silent
+            winget install --id=RustDesk.RustDesk --exact --source winget --silent
+            winget install Microsoft.PowerToys --source winget --silent
+            winget install Notepad++.Notepad++ --source winget --silent
+            winget install 7zip.7zip --source winget --silent
+            winget install ShareX.ShareX --source winget --silent
+            winget install Microsoft.Teams --source winget --silent
+            Write-Host "The process is completed!" -ForegroundColor Cyan
+        }
+        "nerv-nas" {
+            Write-Host "Installing all packages..." -ForegroundColor Red
+            winget install Microsoft.VCRedist.2015+.x64	--source winget --silent
+            winget install Microsoft.VCRedist.2015+.x86	--source winget --silent
+            wsl --install
+            winget install MongoDB.DatabaseTools	--source winget --silent
+            winget install MongoDB.Shell	--source winget --silent
+            winget install MongoDB.Compass.Full	--source winget --silent
+            winget install Docker.DockerDesktop	--source winget --silent
             Write-Host "The process is completed!" -ForegroundColor Cyan
         }
         default { 
